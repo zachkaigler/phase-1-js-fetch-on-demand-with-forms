56263cb363c61c2656d224344d0cfd815ee272a0
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-21 18:20:44 -0400

    commit tree 63f20ba7af56e087a45b07dc75e348a70faa7c73

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
   
+    const inputForm = document.querySelector('form')
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        console.log(event)
+        const input = document.querySelector('input#searchByID')
+
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
